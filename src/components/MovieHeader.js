0434b00195885060c3ab8c53cda25c7fd4b8f8b7
Phil G <fillupgalbraith@gmail.com>
2021-12-07 12:40:57 -0800

    state mapped to props

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const appTitle = props.appTitle;
     const displayFavorites = true;
+
     
     return(<div className="table-title">
         <div className="row">
@@ -19,4 +21,10 @@ const MovieHeader = (props) => {
     </div>);
 }
 
-export default MovieHeader;
+const mapStateToProps = (state) => {
+    return {
+        appTitle: state.appTitle
+    }
+}
+
+export default connect(mapStateToProps)(MovieHeader);
